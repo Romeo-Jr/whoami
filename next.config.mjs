b5bfcca056fee4dac4eff14ac7ca1e0e5a39e150
Romeo <romeoestoy0101@gmail.com>
2024-12-08 11:33:53 +0800

    upload

--- next.config.mjs
+++ next.config.mjs
@@ -1,8 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  output: "export",
-  basePath: "/whoami",
-  assetPrefix: "/whoami",
-};
+const nextConfig = {};
 
 export default nextConfig;
